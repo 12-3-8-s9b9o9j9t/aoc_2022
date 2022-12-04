f41b79612265e1fc9254261090af7b8cfd63a727
Yanis Bouger <yanis.bouger@gmail.com>
2022-12-04 11:29:47 +0100

    j4

---
+++ solutions/j4/j4_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const uint BUFFSIZE = 16;
+
+int main(int argc, char **argv) {
+    FILE *file;
+    if (file = fopen("../../input/j4.txt", "r")) { // on essaie d'ouvrir le fichier
+        char line[BUFFSIZE];
+
+        int overlap = 0;
+
+        int a1 = 0;
+        int b1 = 0;
+        int a2 = 0;
+        int b2 = 0;
+
+        while (fgets(line, BUFFSIZE, file)) { // tant qu'on est pas à la fin du fichier
+            if (sscanf(line, "%d-%d,%d-%d", &a1, &b1, &a2, &b2) == 4) { // on essaie de récupérer les données de la ligne courante
+                if ((a1 <= a2 && a2 <= b1) || (a2 <= a1 && a1 <= b2)) {
+                    overlap += 1;
+                }
+            }
+            else {
+                fprintf(stderr, "Error extracting data\n");
+                break;
+            }
+        }
+        if (ferror(file) != 0) {
+            fprintf(stderr, "Error reading file content\n");
+        }
+        fclose(file);
+
+        printf("In %d assignment pairs, one range contains part the other\n", overlap);
+    }
+    else {
+        fprintf(stderr, "Error opening file\n");
+    }
+    return 0;
+}
