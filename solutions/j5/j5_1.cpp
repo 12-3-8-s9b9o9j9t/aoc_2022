5782ecf1c69a7e07846b5ef5153f5c39d010662e
Yanis Bouger <yanis.bouger@gmail.com>
2022-12-05 15:41:06 +0100

    j5

---
+++ solutions/j5/j5_1.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+int main() {
+    std::ifstream file("../../input/j5.txt");
+
+    if (file) {
+        std::vector<std::string> draw;
+        std::string line;
+        std::vector<std::stack<char>> stacks;
+
+        while (std::getline(file, line) && !line.empty()) {
+            draw.push_back(line);
+        }
+        if (file) {
+            int n = (draw.back().size()+1) /4;
+            draw.pop_back();
+
+            for (int i = 0; i < n; i++) {
+                stacks.push_back(std::stack<char>());
+            }
+
+            while (!draw.empty()) {
+                line = draw.back();
+                draw.pop_back();
+
+                char crate = '\0';
+                for (int i = 0; i < n; i++) {
+                    crate = line[4*i +1];
+                    if (crate != ' ') {
+                        stacks[i].push(crate);
+                    }
+                }
+            }
+            int nb = 0;
+            int from = 0;
+            int to = 0;
+            while (getline(file, line)) {
+                if (sscanf(line.c_str(), "move %d from %d to %d", &nb, &from, &to) == 3) {
+                    for (int i = 0; i < nb; i++) {
+                        stacks[to-1].push(stacks[from-1].top());
+                        stacks[from-1].pop();
+                    }
+                }
+                else {
+                    std::cerr<<"Error extracting data\n";
+                }
+            }
+        }
+        if (file.eof()) {
+            std::string res = "";
+            for (int i = 0; i < stacks.size(); i++) {
+                res += stacks[i].top();
+            }
+            printf("The message is %s\n", res.c_str());
+        }
+        else {
+            std::cerr<<"Error reading file content\n";
+        }
+    }
+    else {
+        std::cerr<<"Error opening file\n";
+    }
+}
